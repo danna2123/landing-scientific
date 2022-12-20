d30d3275842b0c52940bfc3bdeb1b837fcd02a66
danna2123 <ldanna091@gmail.com>
2022-12-20 15:08:46 -0500

    cree las rutas de las paginas

--- src/templates/Header.js
+++ src/templates/Header.js
@@ -1,15 +1,15 @@
 const Header = () => {
     const view = `
-        <div class= "Header-main">
-            <div class="header-logo">
+        <div class="Header-main">
+            <div class="Header-logo">
                 <h1>
                     <a href="/">
                         100tifi.co
                     </a>
                 </h1>
             </div>
-            <div class="Header-nav>
-                <a href="#/about/>
+            <div class="Header-nav">
+                <a href="#/about/">
                     About
                 </a>
             </div>
